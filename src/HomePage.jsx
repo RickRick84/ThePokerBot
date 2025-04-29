8ec1d6c656ab9fd65a5ea1179a9699f7d47a8c48
RickRick84 <flsheandoxahi@gmail.com>
2025-04-29 18:36:44 -0500

    restart de cambios

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,8 +5,6 @@ function HomePage() {
   return (
     <div className="homepage-container">
       <div className="language-selector">
-        <h2>I ❤️ POKER</h2>
-        <p>Ask me anything about it...</p>
         <Link to="/chat?lang=es"><button>ESPAÑOL</button></Link>
         <Link to="/chat?lang=pt"><button>PORTUGUÊS</button></Link>
         <Link to="/chat?lang=en"><button>ENGLISH</button></Link>
